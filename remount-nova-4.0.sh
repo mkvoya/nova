edba39c69e7d6c4460fc64ef69be7085fe5858ae
Andiry Xu <jix024@cs.ucsd.edu>
2015-12-28 17:33:04 -0800

    Add old 4.0 scripts

---
+++ remount-nova-4.0.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Unmount existing partition..."
+umount /mnt/ramdisk
+rmmod nova
+insmod nova.ko measure_timing=0
+
+echo "Unmount done."
+sleep 1
+
+echo "Mounting..."
+mount -t NOVA -o physaddr=0x300000000 NOVA /mnt/ramdisk
+
